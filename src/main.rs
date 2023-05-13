ac95ae5868b8123a1e48a8d349ffa75969884ac4
ckoopmann <c.k.e.koopmann@gmail.com>
2023-05-13 11:56:52 +0800

    Factor out creation of slot_stream into helper function

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,10 @@ async fn run_oracle_node(
     price_provider: impl PriceProvider,
     signature_provider: impl SignatureProvider,
     message_broadcaster: impl MessageBroadcaster,
-    mut epoch_stream: impl Stream<Item = u64> + std::marker::Unpin,
+    mut slot_stream: impl Stream<Item = u64> + std::marker::Unpin,
 ) -> Result<()> {
-    while let Some(epoch_number) = epoch_stream.next().await {
-        println!("Reporting price for epoch: {}", epoch_number);
+    while let Some(slot_number) = slot_stream.next().await {
+        println!("Reporting price for slot: {}", slot_number);
         let price = price_provider.get_price().expect("Error getting price");
         let price_ssz: Vec<u8> = price.as_ssz_bytes();
         let signature = signature_provider.sign(&price_ssz).expect("Error signing");
@@ -26,6 +26,17 @@ async fn run_oracle_node(
     }
 
     Ok(())
+}
+
+async fn get_slot_stream<'a>(provider: &'a Provider<Ws>) -> Result<impl Stream<Item = u64> + 'a> {
+    // Ensures this program exits after a few blocks blocks for easier testing, in production the stream should run forever
+    let num_of_blocks_to_run = 3;
+    let block_stream = provider
+        .subscribe_blocks()
+        .await?
+        .take(num_of_blocks_to_run);
+    let slot_stream = block_stream.map(|block| block.number.unwrap().as_u64());
+    Ok(slot_stream)
 }
 
 #[tokio::main]
@@ -38,23 +49,19 @@ async fn main() -> Result<()> {
     // TODO: Replace with a broadcaster that reports the results to our server
     let message_broadcaster = LogMessageBroadcaster {};
 
-    // TODO: Replace with stream that returns the current epoch instead of mined blocknumbers
+    // TODO: Replace with stream that returns the current slot instead of mined blocknumbers
     let provider =
         Provider::<Ws>::connect("wss://mainnet.infura.io/ws/v3/c60b0bb42f8a4c6481ecd229eddaca27")
             .await?;
-    // Ensures this program exits after a few blocks blocks for easier testing, in production the stream should run forever
-    let num_of_blocks_to_run = 3;
-    let block_stream = provider
-        .subscribe_blocks()
-        .await?
-        .take(num_of_blocks_to_run);
-    let epoch_stream = block_stream.map(|block| block.number.unwrap().as_u64());
+
+    let slot_stream = get_slot_stream(&provider).await?;
+
 
     run_oracle_node(
         price_provider,
         signature_provider,
         message_broadcaster,
-        epoch_stream,
+        slot_stream,
     )
     .await?;
     Ok(())
